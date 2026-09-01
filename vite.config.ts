cf4cb91ac44977793c73e3327096d9c6821b0a95
badal <sraribadal@gmail.com>
2026-09-01 16:08:25 +0530

    aa

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/WorkAdda/', // Essential for GitHub Pages sub-path routing
+  base: './',
 });
